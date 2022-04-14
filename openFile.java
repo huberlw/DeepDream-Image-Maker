820cc7a07e414b7bfd3ecd1a3d8f164b201a48aa
williamsgab <71035801+williamsgab@users.noreply.github.com>
2022-04-13 23:08:41 -0700

    Layer Presets (Styles)
    Replaces layer selection with "dreamification" styles in the form of layer presets.

--- openFile.java
+++ openFile.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.Random;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
@@ -8,43 +9,45 @@ public class openFile {
     public static void main(String[] args) throws IOException {
         
         // create window to select a jpg or png file
-        JFileChooser openFile = new JFileChooser();
+        JFileChooser openFile = new JFileChooser(System.getProperty("user.dir") + "\\input");
         openFile.setAcceptAllFileFilterUsed(false);
         FileNameExtensionFilter extensionFilter = new FileNameExtensionFilter("JPG or PNG file", "jpg", "png");
         openFile.addChoosableFileFilter(extensionFilter);
         int fileOpened = openFile.showOpenDialog(null);
-
-        // layers for Python script
-        int layer1 = -1;
-        int layer2 = -1;
-
-        Scanner userInput = new Scanner(System.in);
-        boolean flag = true;
         
-        // loop to get layers from user
-        // only accepts whole numbers between 0 and 9
-        while(flag) {
-            try {
-                System.out.println("Enter first layer (0-9).");
-                layer1 = Integer.parseInt(userInput.nextLine());
-                if (!(layer1 > -1 && layer1 < 10)) continue;
-                
-                System.out.println("Enter second layer (0-9).");
-                layer2 = Integer.parseInt(userInput.nextLine());
-                if (!(layer1 > -1 && layer1 < 10)) continue;
-                
-                userInput.close();
-                flag = false;
-            } catch (Exception notWholeNumber) {}
-        }
-        
-        // execute the following code if user selected a file
+        // execute the following code if user selects a file
         if(fileOpened == JFileChooser.APPROVE_OPTION) {
+            
+            // styles with different layer combinations
+            int[][] styles = new int[5][2];
+            styles[0] = new int[]{7, 9};
+            styles[1] = new int[]{5, 3};
+            styles[2] = new int[]{3, 8};
+            styles[3] = new int[]{1, 2};
+            styles[4] = new int[]{6, 4};
+
+            int style = -1;
+            Scanner userInput = new Scanner(System.in);
+            boolean flag = true;
+            
+            // loop to get style from user
+            // only accepts whole numbers between 0 and 9
+            while(flag) {
+                try {
+                    System.out.println("Enter a style (0-5).");
+                    style = Integer.parseInt(userInput.nextLine());
+                    if (!(style > -1 && style < 10)) continue;
+                    userInput.close();
+                    flag = false;
+                    System.out.println("Dreamifying...\n");
+                } catch (Exception notWholeNumber) {}
+            }
+            
             // grab path to file
             String path = openFile.getSelectedFile().toString();
 
             // start python script with file path and layers as arguments
-            ProcessBuilder processBuilder = new ProcessBuilder("python", System.getProperty("user.dir") + "\\main.py", path, ""+layer1, ""+layer2);
+            ProcessBuilder processBuilder = new ProcessBuilder("python", System.getProperty("user.dir") + "\\main.py", path, ""+styles[style][0], ""+styles[style][1]);
             Process pythonScript = processBuilder.start();
 
             // read output from script for debugging
