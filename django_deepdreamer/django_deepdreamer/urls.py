3175938aa6a974be3160726ab582cd6453b11b30
huberlw <huberlw1@gmail.com>
2022-04-20 00:33:19 -0700

    limited dreamify + image display
    Can now dreamify images local to project, but not otherwise.
    
    There is also a feature to show an image.

--- django_deepdreamer/django_deepdreamer/urls.py
+++ django_deepdreamer/django_deepdreamer/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .views import index
+from django.urls import path, include
+from . import views
+from . import dreamify
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
+    path('', views.index),
+    path("sweet_dreams", dreamify.sweet_dreams),
 ]
--- django_deepdreamer/react_deepdreamer/src/App.js
+++ django_deepdreamer/react_deepdreamer/src/App.js
@@ -1,14 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Hello World
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state={tmpImg: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png'}
+
+  imgHandler = (e) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      if(reader.readyState === 2){
+        this.setState({tmpImg: reader.result})
+      }
+    }
+    reader.readAsDataURL(e.target.files[0])
+  }
+
+  render() {
+    const {tmpImg} = this.state
+    return (
+      <div className="page">
+        <div clasName="container">
+          <h1 className="heading">Dreamify</h1>
+          <div>
+            <img src={tmpImg} alt="" id="img"></img>
+          </div>
+          <input type="file" className="open" name="img-up" id="input" accept="image\*"
+          onChange={this.imgHandler}/>
+        </div>
+        <form action="/sweet_dreams">
+          <button>Dreamify</button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default App;
