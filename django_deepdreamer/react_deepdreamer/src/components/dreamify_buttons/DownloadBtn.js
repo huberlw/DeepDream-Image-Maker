0662131c1fe67ce2fd4bae07608fd65041fe3335
williamsgab <71035801+nawavon@users.noreply.github.com>
2022-05-04 15:04:49 -0700

    New styling

--- django_deepdreamer/react_deepdreamer/src/components/dreamify_buttons/DownloadBtn.js
+++ django_deepdreamer/react_deepdreamer/src/components/dreamify_buttons/DownloadBtn.js
@@ -3,8 +3,13 @@ import React from 'react'
 
 function DownloadBtn(props) {
     return (
+<<<<<<< Updated upstream
         <button className="DreamifyButtons" onClick={props.onClick}>Download Image</button>
         )
+=======
+        <label className="download-btn" onClick={props.onClick}>Download</label>
+    )
+>>>>>>> Stashed changes
 }
 
 export default DownloadBtn
--- django_deepdreamer/react_deepdreamer/src/components/pages/About.js
+++ django_deepdreamer/react_deepdreamer/src/components/pages/About.js
@@ -5,18 +5,14 @@ export default function About() {
   return (
         <div>
             <div className='about-container'>
-                
-                <div className="inside-about">
-                <h1>ABOUT</h1>
-                </div>
-                <div className="inside-about2">
-                
-                    <p>Dreamify uses machine learning to convert an existing image into something new and exciting! 
-                    Utilizing Google's powerful deepdream technology, dreaming of new images has become easier than ever. 
-                    Click on the "Dreamify" tab to get started.</p>
+                <div className='about-info'>
+                  <h1>ABOUT</h1>
+              
+                  <p>DeepDreamer uses machine learning to convert a normal image into something new and exciting! 
+                      With Google's powerful deepdream technology, dreaming of images is now easier than ever. 
+                      Click on the "Dreamify" tab to get started!</p>
                 </div>
             </div>
-
         </div>
   )
 }
