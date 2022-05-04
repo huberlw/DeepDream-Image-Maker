629a3694ccaf332af9d964e205578b8010ebcf50
williamsgab <71035801+nawavon@users.noreply.github.com>
2022-05-04 16:27:00 -0700

    New styling (fixed)

--- django_deepdreamer/react_deepdreamer/src/components/dreamify_buttons/ResetBtn.js
+++ django_deepdreamer/react_deepdreamer/src/components/dreamify_buttons/ResetBtn.js
@@ -1,17 +1,9 @@
-<<<<<<< Updated upstream
 import './Buttons.css'
-=======
-import './ResetBtn.css'
->>>>>>> Stashed changes
 import React from 'react'
 
 function ResetBtn(props) {
     return (
-<<<<<<< Updated upstream
         <button className="DreamifyButtons" onClick={props.onClick}>Reset</button>
-=======
-        <label className="reset-btn" onClick={props.onClick}>Reset</label>
->>>>>>> Stashed changes
     )
 }
 
