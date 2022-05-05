d22b8ba4615aba98dd91a2052a0894a968483619
williamsgab <71035801+nawavon@users.noreply.github.com>
2022-05-04 20:39:07 -0700

    Revert "New styling (fixed)"
    This reverts commit 629a3694ccaf332af9d964e205578b8010ebcf50.

--- django_deepdreamer/react_deepdreamer/src/components/dreamify_buttons/DreamifyBtn.js
+++ django_deepdreamer/react_deepdreamer/src/components/dreamify_buttons/DreamifyBtn.js
@@ -3,8 +3,13 @@ import React from 'react'
 
 function DreamifyBtn(props) {
     return (
+<<<<<<< Updated upstream
         <button className="DreamifyButtons" onClick={props.onClick}>Dreamify</button>
         )
+=======
+        <label className="dream-btn" onClick={props.onClick}>Dreamify</label>
+    )
+>>>>>>> Stashed changes
 }
 
 export default DreamifyBtn
